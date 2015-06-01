d998f24794030013977ab1907edcda47cbf9faf8
Jeff Wadden <jwadden@missingcomma.com>
2015-06-01 00:58:02 -0400

    Created settings template file.

---
+++ settings_template.py
@@ -0,0 +1,19 @@
+db_engine = ''
+salt = ''
+debug = False
+
+name = ''
+phone = ''
+email = ''
+recipient_name = ''
+recipient_company = ''
+
+rate = 0
+
+note = (
+'''
+'''
+)
+
+address_parts = [
+]
